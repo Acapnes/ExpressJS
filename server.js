3baac61cc659fc2b85dd650ba8310ad3fe36b42d
Alper <61701011+Acapnes@users.noreply.github.com>
2022-05-06 15:28:20 +0300

    Update user with Put req

--- server.js
+++ server.js
@@ -12,33 +12,41 @@ const users = [
   { id: 2, name: "Omer", password: "321" },
 ];
 
-
-
-
-app.post("/users",(req,res,next) => {
+app.post("/users", (req, res, next) => {
   users.push(req.body);
   res.json({
     success: true,
     data: users,
-    response: "New user created"
+    response: "New user created",
   });
 });
 
-app.delete("/users",(req,res,next) => {
+app.delete("/users", (req, res, next) => {
   res.json({
     success: true,
     comment: "Delete Request",
   });
 });
 
-app.put("/users",(req,res,next) => {
+app.put("/users/:id", (req, res, next) => {
+  const id = parseInt(req.params.id);
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].id === id) {
+      users[i] = {
+        ...users[i],
+        ...req.body,
+      };
+    }
+  }
   res.json({
     success: true,
+    data: users,
     comment: "Put Request",
   });
 });
 
-app.get("/products", accessControl, (req, res, next) => { /// sadece belirttiğimiz request için çalışıyor.
+app.get("/products", accessControl, (req, res, next) => {
+  /// sadece belirttiğimiz request için çalışıyor.
   res.send("Products");
 });
 
@@ -47,8 +55,6 @@ app.get("/users", (req, res, next) => {
 });
 
 app.use(accessControl); /// Altındaki bütün requestler için çalışıyor
-
-
 
 app.listen(PORT, () => {
   console.log("Server Started!");
