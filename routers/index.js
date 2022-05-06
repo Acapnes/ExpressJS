2c2f490646b7a27458f09ca9b4a3b77c828cdd8d
Alper <61701011+Acapnes@users.noreply.github.com>
2022-05-06 17:21:36 +0300

    Include routing

---
+++ routers/index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const authRouter = require("./auth");
+const questionRouter = require("./question");
+const router = express.Router();
+
+router.use("/users",authRouter);
+router.use("/question",questionRouter);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,68 +1,21 @@
 const express = require("express");
 const { accessControl } = require("./middleware");
+const routers = require("./routers");
+const dotenv = require("dotenv");
+
 
 const app = express();
 
-const PORT = 5000;
+dotenv.config({
+  path : "./config/env/config.env"
+});
 
 app.use(express.json());
 
-const users = [
-  { id: 1, name: "Alper", password: "123" },
-  { id: 2, name: "Omer", password: "321" },
-];
+/// Routers
+app.use("",routers);
 
-app.post("/users", (req, res, next) => {
-  users.push(req.body);
-  res.json({
-    success: true,
-    data: users,
-    response: "New user created",
-  });
-});
-
-app.delete("/users/:id", (req, res, next) => {
-  const id = parseInt(req.params.id);
-  for (let i = 0; i < users.length; i++) {
-    if (users[i].id === id) {
-      users.splice(i,1);
-    }
-  }
-  res.json({
-    success: true,
-    data: users,
-    comment: "Delete Request",
-  });
-});
-
-app.put("/users/:id", (req, res, next) => {
-  const id = parseInt(req.params.id);
-  for (let i = 0; i < users.length; i++) {
-    if (users[i].id === id) {
-      users[i] = {
-        ...users[i],
-        ...req.body,
-      };
-    }
-  }
-  res.json({
-    success: true,
-    data: users,
-    comment: "Put Request",
-  });
-});
-
-app.get("/products", accessControl, (req, res, next) => {
-  /// sadece belirttiğimiz request için çalışıyor.
-  res.send("Products");
-});
-
-app.get("/users", (req, res, next) => {
-  res.json(users);
-});
-
-app.use(accessControl); /// Altındaki bütün requestler için çalışıyor
-
-app.listen(PORT, () => {
+// app.use(accessControl); /// Altındaki bütün requestler için çalışıyor
+app.listen(process.env.PORT, () => {
   console.log("Server Started!");
 });
